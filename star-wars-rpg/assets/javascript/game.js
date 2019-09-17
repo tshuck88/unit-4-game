862f4e99dc8e24f7dc06668c7749fc4942eab50b
Tyler Shuck <tshuck88@gmail.com>
2019-09-17 12:25:52 -0700

    created initial objects. put characters inside containers and floated them left. created clearfix hack

---
+++ star-wars-rpg/assets/javascript/game.js
@@ -0,0 +1,27 @@
+var obiWan = {
+    name: "Obi-Wan Kenobi",
+    healthPoints: 120,
+    attackPower: 8,
+    counterAttackPower: 8
+};
+
+var lukeSkywalker = {
+    name: "Luke Skywalker",
+    healthPoints: 100,
+    attackPower: 10,
+    counterAttackPower: 5
+};
+
+var darthVader = {
+    name: "Darth Vader",
+    healthPoints: 150,
+    attackPower: 6,
+    counterAttackPower: 20
+};
+
+var darthMaul = {
+    name: "Darth Maul",
+    healthPoints: 150,
+    attackPower: 4,
+    counterAttackPower: 25
+};
