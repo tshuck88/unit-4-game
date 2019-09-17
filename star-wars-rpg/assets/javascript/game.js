89f8d88602e2c7f91497ff7fe019b6e3dc169bb9
Tyler Shuck <tshuck88@gmail.com>
2019-09-17 13:41:53 -0700

    added a reset.css file. added and changed some class in index. created a border around the character images. created a function to display character name and hp

--- star-wars-rpg/assets/javascript/game.js
+++ star-wars-rpg/assets/javascript/game.js
@@ -1,27 +1,48 @@
-var obiWan = {
-    name: "Obi-Wan Kenobi",
-    healthPoints: 120,
-    attackPower: 8,
-    counterAttackPower: 8
-};
+$(document).ready(function () {
+    var obiWan = {
+        name: "Obi-Wan Kenobi",
+        healthPoints: 120,
+        attackPower: 8,
+        counterAttackPower: 8
+    };
 
-var lukeSkywalker = {
-    name: "Luke Skywalker",
-    healthPoints: 100,
-    attackPower: 10,
-    counterAttackPower: 5
-};
+    var lukeSkywalker = {
+        name: "Luke Skywalker",
+        healthPoints: 100,
+        attackPower: 10,
+        counterAttackPower: 5
+    };
 
-var darthVader = {
-    name: "Darth Vader",
-    healthPoints: 150,
-    attackPower: 6,
-    counterAttackPower: 20
-};
+    var darthVader = {
+        name: "Darth Vader",
+        healthPoints: 150,
+        attackPower: 6,
+        counterAttackPower: 20
+    };
 
-var darthMaul = {
-    name: "Darth Maul",
-    healthPoints: 150,
-    attackPower: 4,
-    counterAttackPower: 25
-};
+    var darthMaul = {
+        name: "Darth Maul",
+        healthPoints: 150,
+        attackPower: 4,
+        counterAttackPower: 25
+    };
+
+    function displayGameText() {
+        $(".obi-wan-kenobi.character-name").text(obiWan.name);
+        $(".obi-wan-kenobi.character-hp").text(obiWan.healthPoints);
+        $(".luke-skywalker.character-name").text(lukeSkywalker.name);
+        $(".luke-skywalker.character-hp").text(lukeSkywalker.healthPoints);
+        $(".darth-vader.character-name").text(darthVader.name);
+        $(".darth-vader.character-hp").text(darthVader.healthPoints);
+        $(".darth-maul.character-name").text(darthMaul.name);
+        $(".darth-maul.character-hp").text(darthMaul.healthPoints);
+
+    };
+    displayGameText();
+
+    $(".your-character").on("click", function(){
+       var yourCharacter = $(this).appendTo("#your-character-area") 
+    });
+
+
+});
