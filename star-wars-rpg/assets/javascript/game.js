0bc7cb63c0902cf26d5a9aabd5da9696935acddc
Tyler Shuck <tshuck88@gmail.com>
2019-09-18 10:45:11 -0700

    created ids for all characters. added styling for chosen enemies. created array of objects and a function to select the objects.

--- star-wars-rpg/assets/javascript/game.js
+++ star-wars-rpg/assets/javascript/game.js
@@ -1,31 +1,72 @@
 $(document).ready(function () {
+
+    var characterList = [
+        {
+            name: "Obi-Wan Kenobi",
+            healthPoints: 120,
+            attackPower: 8,
+            counterAttackPower: 8,
+            characterID: "obi-wan-kenobi"
+        },
+        {
+            name: "Luke Skywalker",
+            healthPoints: 100,
+            attackPower: 10,
+            counterAttackPower: 5,
+            characterID: "luke-skywalker"
+        },
+        {
+            name: "Darth Vader",
+            healthPoints: 150,
+            attackPower: 6,
+            counterAttackPower: 20,
+            characterID: "darth-vader"
+        },
+        {
+            name: "Darth Maul",
+            healthPoints: 150,
+            attackPower: 4,
+            counterAttackPower: 25,
+            characterID: "darth-maul"
+        }
+    ];
     var obiWan = {
         name: "Obi-Wan Kenobi",
         healthPoints: 120,
         attackPower: 8,
-        counterAttackPower: 8
+        counterAttackPower: 8,
+        characterID: "obi-wan-kenobi"
     };
 
     var lukeSkywalker = {
         name: "Luke Skywalker",
         healthPoints: 100,
         attackPower: 10,
-        counterAttackPower: 5
+        counterAttackPower: 5,
+        characterID: "luke-skywalker"
     };
 
     var darthVader = {
         name: "Darth Vader",
         healthPoints: 150,
         attackPower: 6,
-        counterAttackPower: 20
+        counterAttackPower: 20,
+        characterID: "darth-vader"
     };
 
     var darthMaul = {
         name: "Darth Maul",
         healthPoints: 150,
         attackPower: 4,
-        counterAttackPower: 25
+        counterAttackPower: 25,
+        characterID: "darth-maul"
     };
+
+    var userCharacter;
+    var userCharacterID;
+    var enemyCharacterID;
+    var chosenEnemeyCharacter;
+    var isEnemyChosen = false;
 
     function displayGameText() {
         $(".obi-wan-kenobi.character-name").text(obiWan.name);
@@ -36,21 +77,32 @@ $(document).ready(function () {
         $(".darth-vader.character-hp").text(darthVader.healthPoints);
         $(".darth-maul.character-name").text(darthMaul.name);
         $(".darth-maul.character-hp").text(darthMaul.healthPoints);
-
-    };
+    }
     displayGameText();
 
-    $(".character").on("click", function(){
-       var userCharacter = $(this).appendTo("#your-character-area");
-       $(userCharacter).addClass("your-character").removeClass("character");
-       $("#character-container").appendTo("#enemies-available-to-attack");
-       $(".character").addClass("enemy-character").removeClass("character");
+    $(document).on("click", ".character", function () {
+        userCharacter = $(this).appendTo("#your-character-area");
+        userCharacterID = $(this).attr("id");
+        $(userCharacter).addClass("your-character").removeClass("character");
+        $("#character-container").appendTo("#enemies-available-to-attack");
+        $(".character").addClass("enemy-character").removeClass("character");
     });
 
-    $(".enemy-character").on("click", function(){
-        var chosenEnemeyCharacter = $(this).appendTo("#defender-area");
-        $(chosenEnemeyCharacter).addClass("chosen-enemy")
+    $(document).on("click", ".enemy-character", function () {
+        if (!isEnemyChosen) {
+            chosenEnemeyCharacter = $(this).appendTo("#defender-area");
+            enemyCharacterID = $(this).attr("id");
+            $(chosenEnemeyCharacter).addClass("chosen-enemy");
+            isEnemyChosen = true;
+        }
     });
-
+    $(document).on("click", "#attack-button", function () {
+        userCharacter = $.grep(characterList, function (obj) {
+            return obj.characterID === userCharacterID
+        });
+        chosenEnemeyCharacter = $.grep(characterList, function(obj){
+            return obj.characterID === enemyCharacterID
+        });
+    });
 
 });
